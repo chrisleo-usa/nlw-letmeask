0f71b0ae179cda45e2ec93273322ff19a244a705
Christopher Alves <christopheralves@portalsolar.com.br>
2021-07-13 09:14:29 -0300

    refactored style and logic in Home and New Room page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,13 @@
 import { FormEvent, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { database } from '../services/firebase'
-
 import { useAuth } from '../hooks/useAuth'
-import { Button } from '../components/Button/Button'
+
+import { Banner } from '../components/Banner'
+import { FormNewRoom } from '../components/FormNewRoom'
 
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
-import { Banner } from '../components/Banner'
 
 import '../styles/auth.scss'
 
@@ -47,25 +47,22 @@ export const Home = () => {
   }
 
   return (
-    <div id="page-auth" className="flex flex-col">
+    <div id="page-auth">
       <Banner />
       <main>
-        <div className="main-content p-4">
-          <img className="self-center" src={logoImg} alt="Letmeask" />
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
           <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div> 
-          <form onSubmit={handleJoinRoom}>
-            <input
-              type="text" 
-              placeholder="Digite o código da sala" 
-              onChange={event => setRoomCode(event.target.value)}
-              value={roomCode}
-            />
-            <Button type="submit">Entrar na sala</Button>
-          </form>
+          <FormNewRoom 
+            onSubmit={handleJoinRoom} 
+            onChange={setRoomCode} 
+            inputValue={roomCode} 
+            inputPlaceholder="Digite o código da sala"
+            btnText="Entrar na sala" />
         </div>
       </main>
     </div>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,14 @@
 import { FormEvent, useState } from 'react'
+import { database } from '../services/firebase'
 import { useAuth } from '../hooks/useAuth'
-
-import { Button } from '../components/Button/Button'
 import { Link, useHistory } from 'react-router-dom'
 
-import illustrationImg from '../assets/images/illustration.svg'
+import { Banner } from '../components/Banner'
+import { FormNewRoom } from '../components/FormNewRoom'
+
 import logoImg from '../assets/images/logo.svg'
 
 import '../styles/auth.scss'
-import { database } from '../services/firebase'
 
 export const NewRoom = () => {
   const { user } = useAuth()
@@ -34,25 +34,20 @@ export const NewRoom = () => {
 
   return (
     <div id="page-auth">
-      <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-        <strong>Crie salas de Q&amp;A ao vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo real</p>
-      </aside>
+      <Banner />
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form onSubmit={handleCreateRoom}>
-            <input 
-              type="text" 
-              placeholder="Nome da sala"
-              onChange={event => setNewRoom(event.target.value)} 
-              value={newRoom} 
+          <FormNewRoom 
+            onSubmit={handleCreateRoom} 
+            onChange={setNewRoom} 
+            inputValue={newRoom} 
+            inputPlaceholder="Nome da sala"
+            btnText="Criar sala" 
+            className="formCreateRoom"
             />
-            <Button type="submit">Criar sala</Button>
-          </form>
-          <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link></p>
+          <span>Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link></span>
         </div>
       </main>
     </div>
